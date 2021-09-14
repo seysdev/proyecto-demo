482cc5e6e90217dc8d0843a5bb3acfb5f9d33e7f
sebastian yabiku <sby2710@gmail.com>
2021-09-13 19:29:31 -0500

    se agrego componente header y nav

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import { Nav } from "components/nav";
+
+export function Header() {
+  return (
+    <header className="header">
+      <strong className="header__logo">LOGO</strong>
+      <Nav />
+    </header>
+  );
+}
